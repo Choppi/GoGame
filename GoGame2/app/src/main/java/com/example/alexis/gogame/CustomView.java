fb7d1c58729a22bb1e2aa283f8a0c55542549a2b
Genetter <rocky.smith@outlook.fr>
2016-11-24 16:17:36 +0000

    ajout CustomView.java:     -les 3 constructeurs     -methodes singleMeasure et onMeasure pour définir la taille de la custom view     -onDraw() dessins de la grille (à tester)     -dans les attributs : Paint black : couleur de la grille initialisé dans init()

---
+++ GoGame2/app/src/main/java/com/example/alexis/gogame/CustomView.java
@@ -0,0 +1,115 @@
+// imports
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.lang.String.valueOf;
+
+//class definition
+public class CustomView extends View {
+
+    Paint black;
+
+
+    //default constructor
+    public CustomView(Context c) {
+        super(c);
+        init();
+    }
+
+    //constructor that takes in a context and also a list of attributes
+//that were set through XML
+    public CustomView(Context c, AttributeSet as) {
+        super(c, as);
+        init();
+    }
+
+    // constructor that take in a context, attribute set and also a default
+// style in case the view is to be styled in a certian way
+    public CustomView(Context c, AttributeSet as, int default_style) {
+        super(c, as, default_style);
+        init();
+    }
+
+    private int singleMeasure(int spec, int screenDim) {
+        /*
+          Mesure sur un axe
+
+         */
+        int mode = MeasureSpec.getMode(spec);
+        int size = MeasureSpec.getSize(spec);
+        // Si le layout n'a pas précisé de dimensions, la vue prendra la moitié de l'écran
+        if(mode == MeasureSpec.UNSPECIFIED)
+            return screenDim/2;
+        else
+            // Sinon, elle prendra la taille demandée par le layout
+            return size;
+    }
+
+
+    @Override
+
+    protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec) {
+        // On récupère les dimensions de l'écran
+        DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+        // Sa largeur…
+        int screenWidth = metrics.widthPixels;
+        // … et sa hauteur
+        int screenHeight = metrics.heightPixels;
+        int retourWidth = singleMeasure(widthMeasureSpec, screenWidth);
+        int retourHeight = singleMeasure(heightMeasureSpec, screenHeight);
+        // Comme on veut un carré, on n'aura qu'une taille pour les deux axes, la plus petite possible
+        int retour = Math.min(retourWidth, retourHeight);
+        setMeasuredDimension(retour, retour);
+    }
+
+    public void init() {
+        balck = new Paint(Paint.ANTI_ALIAS_FLAG);
+        balck.setColor(Color.BLACK);
+    }
+
+    public void onDraw(final Canvas canvas){
+        //call the superclass method
+        super.onDraw(canvas);
+
+        // Largeur de la vue
+        int width = getWidth();
+        // Hauteur de la vue
+        int height = getHeight();
+        step = Math.min(width, height);
+
+        float tranche = (float)step*0.1f;
+
+        canvas.drawRect(step * 0.125f, step * 0.125f, step * 0.875f, step * 0.875f, black);
+
+        int i=1;
+        while (i < 10){
+            canvas.drawLine(step * 0.125f+tranche*i, step * 0.125f,step * 0.125f+tranche*i, step * 0.875f, black);
+            i++;
+        }
+        int i=1;
+        while (i < 10){
+            canvas.drawLine(step * 0.125f, step * 0.125f+tranche*i, step * 0.875f, step * 0.125f+tranche*i, black);
+            i++
+        }
+
+
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+
+    }
+}
+
